c8b3727c6a063f14d1f04e3f99fe35c34a0990e1
Marcos Lucas <marcosvaldeni@gmail.com>
2021-08-02 16:09:16 +0100

    migration fix

---
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,29 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+  
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
